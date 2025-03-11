3e817e7abd62d90acd3b819c4a59d9db3de24c12
kierra.searle@ibm.com <kierra.searle@ibm.com>
2025-03-11 11:42:18 -0400

    feat: advanced example

--- examples/advanced/README.md
+++ examples/advanced/README.md
@@ -2,3 +2,10 @@
 
 <!-- There is a pre-commit hook that will take the title of each example add include it in the repos main README.md  -->
 <!-- Add text below should describe exactly what resources are provisioned / configured by the example  -->
+
+Example that configures:
+
+- COS instance and KMS encrypted COS buckets
+- Cloud Logs with Event Notifications integration
+- Cloud Logs policies
+- Key Protect instance and root key
--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -10,23 +10,163 @@ module "resource_group" {
   existing_resource_group_name = var.resource_group
 }
 
+##############################################################################
+# Key Protect Instance + Key (used to encrypt bucket)
+##############################################################################
+
+locals {
+  key_ring_name = "${var.prefix}-cloud-logs"
+  key_name      = "${var.prefix}-cloud-logs-key"
+}
+
+module "key_protect" {
+  source            = "terraform-ibm-modules/kms-all-inclusive/ibm"
+  version           = "4.20.0"
+  resource_group_id = module.resource_group.resource_group_id
+  region            = var.region
+  resource_tags     = var.resource_tags
+  keys = [
+    {
+      key_ring_name = local.key_ring_name
+      keys = [
+        {
+          key_name = local.key_name
+        }
+      ]
+    }
+  ]
+  key_protect_instance_name = "${var.prefix}-kp"
+}
+
+##############################################################################
+# Event Notification
+##############################################################################
+
+module "event_notification_1" {
+  source            = "terraform-ibm-modules/event-notifications/ibm"
+  version           = "1.18.8"
+  resource_group_id = module.resource_group.resource_group_id
+  name              = "${var.prefix}-en-1"
+  tags              = var.resource_tags
+  plan              = "standard"
+  service_endpoints = "public"
+  region            = var.region
+}
+
+module "event_notification_2" {
+  source            = "terraform-ibm-modules/event-notifications/ibm"
+  version           = "1.18.8"
+  resource_group_id = module.resource_group.resource_group_id
+  name              = "${var.prefix}-en-2"
+  tags              = var.resource_tags
+  plan              = "standard"
+  service_endpoints = "public"
+  region            = var.region
+}
+
+##############################################################################
+# COS instance + buckets
+##############################################################################
+
+module "cos" {
+  source            = "terraform-ibm-modules/cos/ibm"
+  version           = "8.19.5"
+  resource_group_id = module.resource_group.resource_group_id
+  cos_instance_name = "${var.prefix}-cos"
+  cos_tags          = var.resource_tags
+  create_cos_bucket = false
+}
+
+locals {
+  logs_bucket_name    = "${var.prefix}-logs-data"
+  metrics_bucket_name = "${var.prefix}-metrics-data"
+}
+
+module "buckets" {
+  source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
+  version = "8.19.5"
+  bucket_configs = [
+    {
+      bucket_name                   = local.logs_bucket_name
+      kms_encryption_enabled        = true
+      region_location               = var.region
+      resource_instance_id          = module.cos.cos_instance_id
+      kms_guid                      = module.key_protect.kms_guid
+      kms_key_crn                   = module.key_protect.keys["${local.key_ring_name}.${local.key_name}"].crn
+      skip_iam_authorization_policy = false
+    },
+    {
+      bucket_name                   = local.metrics_bucket_name
+      kms_encryption_enabled        = true
+      region_location               = var.region
+      resource_instance_id          = module.cos.cos_instance_id
+      kms_guid                      = module.key_protect.kms_guid
+      kms_key_crn                   = module.key_protect.keys["${local.key_ring_name}.${local.key_name}"].crn
+      skip_iam_authorization_policy = true # Auth policy created in first bucket
+    }
+  ]
+}
+
 ########################################################################################################################
-# COS
+# Cloud Logs
 ########################################################################################################################
 
 #
 # Developer tips:
 #   - Call the local module / modules in the example to show how they can be consumed
-#   - Include the actual module source as a code comment like below so consumers know how to consume from correct location
+#   - include the actual module source as a code comment like below so consumers know how to consume from correct location
 #
 
-module "cos" {
-  source = "../.."
-  # remove the above line and uncomment the below 2 lines to consume the module from the registry
-  # source            = "terraform-ibm-modules/<replace>/ibm"
-  # version           = "X.Y.Z" # Replace "X.Y.Z" with a release version to lock into a specific release
-  name              = "${var.prefix}-cos"
+locals {
+  cloud_logs_instance_name = "${var.prefix}-cloud-logs"
+}
+
+module "cloud_logs" {
+  source = "../../"
+  # delete line above and use below syntax to pull module source from hashicorp when consuming this module
+  # source    = "terraform-ibm-modules/cloud-logs/ibm"
+  # version   = "X.Y.Z" # Replace "X.X.X" with a release version to lock into a specific release
   resource_group_id = module.resource_group.resource_group_id
+  region            = var.region
+  instance_name     = local.cloud_logs_instance_name
   resource_tags     = var.resource_tags
-  plan              = "cos-one-rate-plan"
+  access_tags       = var.access_tags
+  data_storage = {
+    # logs and metrics buckets must be different
+    logs_data = {
+      enabled         = true
+      bucket_crn      = module.buckets.buckets[local.logs_bucket_name].bucket_crn
+      bucket_endpoint = module.buckets.buckets[local.logs_bucket_name].s3_endpoint_direct
+    },
+    metrics_data = {
+      enabled         = true
+      bucket_crn      = module.buckets.buckets[local.metrics_bucket_name].bucket_crn
+      bucket_endpoint = module.buckets.buckets[local.metrics_bucket_name].s3_endpoint_direct
+    }
+  }
+  policies = [{
+    logs_policy_name     = "${var.prefix}-logs-policy-1"
+    logs_policy_priority = "type_low"
+    application_rule = [{
+      name         = "test-system-app"
+      rule_type_id = "start_with"
+    }]
+    log_rules = [{
+      severities = ["info", "debug"]
+    }]
+    subsystem_rule = [{
+      name         = "test-sub-system"
+      rule_type_id = "start_with"
+    }]
+  }]
+  existing_en_instances = [{
+    en_instance_id      = module.event_notification_1.guid
+    en_region           = var.region
+    en_integration_name = "${var.prefix}-en-1"
+    },
+    {
+      en_instance_id      = module.event_notification_2.guid
+      en_region           = var.region
+      en_integration_name = "${var.prefix}-en-2"
+  }]
 }
