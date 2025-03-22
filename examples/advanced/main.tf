28d096471da81f13f58e6e5f22c31d397b26c393
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-03-22 12:12:54 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v8.21.1 (#16)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -70,7 +70,7 @@ module "event_notification_2" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "8.21.0"
+  version           = "8.21.1"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
@@ -84,7 +84,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "8.21.0"
+  version = "8.21.1"
   bucket_configs = [
     {
       bucket_name                   = local.logs_bucket_name
