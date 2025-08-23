36a2284c3d4e5709f22869e5638e9b9df3407657
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-08-23 08:48:04 +0530

    fix(deps): update dependencies (#119)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.2.4"
+  version           = "10.2.5"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
@@ -34,7 +34,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "10.2.4"
+  version = "10.2.5"
   bucket_configs = [
     {
       bucket_name            = local.logs_bucket_name
