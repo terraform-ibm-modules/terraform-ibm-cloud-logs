f80cb98b8a1c78706f520536f4641b6f75401d01
Aatreyee Mukherjee <mukherjeeaatreyee25@gmail.com>
2025-11-17 11:11:04 +0530

    Update solutions/fully-configurable/outputs.tf
    Co-authored-by: Md Anam Raihan <md.anam.raihan17@gmail.com>

--- solutions/fully-configurable/outputs.tf
+++ solutions/fully-configurable/outputs.tf
@@ -63,7 +63,7 @@ output "next_steps_text" {
 }
 
 output "next_step_primary_label" {
-  value       = "Go to Cloud Logs instance"
+  value       = "Go to Cloud Logs"
   description = "Primary label"
 }
 
