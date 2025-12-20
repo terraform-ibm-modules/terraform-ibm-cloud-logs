8bfdd80a590d7bdfa1c796be573dfb8afc299414
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-20 04:23:15 +0000

    fix(deps): update dependencies (#186)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -270,7 +270,7 @@ module "kms" {
   }
   count                       = local.use_kms_module ? 1 : 0
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "5.5.4"
+  version                     = "5.5.5"
   create_key_protect_instance = false
   region                      = local.kms_region
   existing_kms_instance_crn   = var.existing_kms_instance_crn
