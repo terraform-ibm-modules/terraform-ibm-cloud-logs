5226006e74579f646080b6b033bf739cbeab921f
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-12-21 12:57:15 +0000

    fix(deps): update dependencies

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -95,7 +95,7 @@ module "buckets" {
   }
   depends_on = [time_sleep.wait_for_authorization_policy[0]]
   source     = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version    = "10.7.5"
+  version    = "10.7.6"
   bucket_configs = [
     {
       bucket_name              = local.data_bucket_name
@@ -270,7 +270,7 @@ module "kms" {
   }
   count                       = local.use_kms_module ? 1 : 0
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "5.5.8"
+  version                     = "5.5.9"
   create_key_protect_instance = false
   region                      = local.kms_region
   existing_kms_instance_crn   = var.existing_kms_instance_crn
