b244a79ca9e235b9b27e56ab69b08aafd77ab3f9
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-16 21:02:46 +0000

    fix(deps): update dependencies

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -73,7 +73,7 @@ module "event_notification_2" {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.8.3"
+  version           = "10.8.5"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.prefix}-cos"
   cos_tags          = var.resource_tags
@@ -87,7 +87,7 @@ locals {
 
 module "buckets" {
   source  = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version = "10.8.3"
+  version = "10.8.5"
   bucket_configs = [
     {
       bucket_name                   = local.logs_bucket_name
