df9d227b1be457d833dd6e813a185053d9cde27c
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2026-02-11 17:19:48 +0530

    resolve review comments

--- solutions/security-enforced/variables.tf
+++ solutions/security-enforced/variables.tf
@@ -49,15 +49,6 @@ variable "ibmcloud_cos_api_key" {
 variable "existing_cos_instance_crn" {
   type        = string
   description = "The CRN of an existing Object Storage instance."
-  validation {
-    condition     = var.existing_cloud_logs_crn == null ? (var.existing_cos_instance_crn != null && var.existing_cos_instance_crn != "") : true
-    error_message = "A value should be passed for 'existing_cos_instance_crn' when 'existing_cloud_logs_crn' is not provided."
-  }
-
-  validation {
-    condition     = var.existing_cloud_logs_crn != null ? var.existing_cos_instance_crn == null : true
-    error_message = "A value should not be passed for 'existing_cos_instance_crn' when passing an existing Cloud Logs instance using the 'existing_cloud_logs_crn' input."
-  }
 }
 
 variable "cloud_logs_data_cos_bucket_name" {
