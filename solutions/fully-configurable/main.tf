fd679977fefa3be447a6c4c6a6df2620b6990fe2
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-05-23 06:40:07 +0530

    fix(deps): update dependencies (#258)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.6.0"
+  version                      = "1.6.1"
   existing_resource_group_name = var.existing_resource_group_name
 }
 
@@ -85,7 +85,7 @@ locals {
 
 module "existing_cos_instance_crn_parser" {
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.5.0"
+  version = "1.6.0"
   crn     = var.existing_cos_instance_crn
 }
 
@@ -95,7 +95,7 @@ module "buckets" {
   }
   depends_on = [time_sleep.wait_for_authorization_policy[0]]
   source     = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version    = "10.16.0"
+  version    = "10.16.1"
   bucket_configs = [
     {
       bucket_name              = local.data_bucket_name
@@ -148,7 +148,7 @@ module "buckets" {
 module "bucket_crns" {
   for_each = module.buckets.buckets
   source   = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version  = "1.5.0"
+  version  = "1.6.0"
   crn      = each.value.bucket_id
 }
 
@@ -204,14 +204,14 @@ resource "time_sleep" "wait_for_cos_authorization_policy" {
 module "existing_kms_crn_parser" {
   count   = var.existing_kms_instance_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.5.0"
+  version = "1.6.0"
   crn     = var.existing_kms_instance_crn
 }
 
 module "existing_kms_key_crn_parser" {
   count   = var.existing_kms_key_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.5.0"
+  version = "1.6.0"
   crn     = var.existing_kms_key_crn
 }
 
