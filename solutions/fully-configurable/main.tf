6fa754a35fb836223c3eed6b56766fb71f5b9818
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-07-12 00:00:22 +0000

    fix(deps): update dependencies

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -95,7 +95,7 @@ module "buckets" {
   }
   depends_on = [time_sleep.wait_for_authorization_policy[0]]
   source     = "terraform-ibm-modules/cos/ibm//modules/buckets"
-  version    = "10.17.4"
+  version    = "10.17.5"
   bucket_configs = [
     {
       bucket_name              = local.data_bucket_name
