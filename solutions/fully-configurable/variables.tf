9b6f3d41be1802d28c41d4894de293155e2ae4e8
Yuvraj Singh <Yuvraj.Singh3@ibm.com>
2026-07-27 00:55:56 +0530

    feat: add support for ICL's standard-plus-metrics plan

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -258,6 +258,17 @@ variable "ibmcloud_kms_api_key" {
 # Cloud Logs
 ########################################################################################################################
 
+variable "cloud_logs_plan" {
+  type        = string
+  description = "The IBM Cloud Logs plan to provision. Available plans: `standard`, `standard-plus-metrics`. Note: `standard-plus-metrics` is not a GAed plan."
+  default     = "standard"
+
+  validation {
+    condition     = contains(["standard", "standard-plus-metrics"], var.cloud_logs_plan)
+    error_message = "The plan value must be one of the following: standard, standard-plus-metrics."
+  }
+}
+
 variable "existing_cloud_logs_crn" {
   type        = string
   default     = null
