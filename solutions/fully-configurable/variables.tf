cc49fcfd2b96afba663bcbebb20e3f9782b84d97
raagamounica <mounica.tellabati@ibm.com>
2026-08-17 22:28:37 +0530

    feat: Removes all references to Hyper Protect Crypto Services for cloud logs

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -179,14 +179,14 @@ variable "kms_encryption_enabled_buckets" {
 variable "existing_kms_instance_crn" {
   type        = string
   default     = null
-  description = "The CRN of an existing KMS instance (Hyper Protect Crypto Services or Key Protect). Used to create a new KMS key unless an existing key is passed using the `existing_cloud_logs_cos_kms_key_crn` input. If the KMS instance is in different account you must also provide a value for `ibmcloud_kms_api_key`. A value should not be passed passing existing Cloud Logs instance using the `existing_cloud_logs_crn` input."
+  description = "The CRN of an existing Key Protect instance. Used to create a new KMS key unless an existing key is passed using the `existing_kms_key_crn` input. If the KMS instance is in different account you must also provide a value for `ibmcloud_kms_api_key`. A value should not be passed when passing existing Cloud Logs instance using the `existing_cloud_logs_crn` input."
 
   validation {
     condition = anytrue([
-      can(regex("^crn:(.*:){3}(kms|hs-crypto):(.*:){2}[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}::$", var.existing_kms_instance_crn)),
+      can(regex("^crn:(.*:){3}(kms):(.*:){2}[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}::$", var.existing_kms_instance_crn)),
       var.existing_kms_instance_crn == null,
     ])
-    error_message = "The provided KMS instance CRN in the input 'existing_kms_instance_crn' in not valid."
+    error_message = "The provided Key Protect instance CRN in the input 'existing_kms_instance_crn' is not valid."
   }
 
   validation {
@@ -198,14 +198,14 @@ variable "existing_kms_instance_crn" {
 variable "existing_kms_key_crn" {
   type        = string
   default     = null
-  description = "The CRN of an existing KMS key to use to encrypt both of the Cloud Logs Object Storage buckets. If no value is set for this variable and bucket encryption is desired, specify a value for the `existing_kms_instance_crn` variable to create a key ring and key."
+  description = "The CRN of an existing Key Protect key to use to encrypt both of the Cloud Logs Object Storage buckets. If no value is set for this variable and bucket encryption is desired, specify a value for the `existing_kms_instance_crn` variable to create a key ring and key."
 
   validation {
     condition = anytrue([
-      can(regex("^crn:(.*:){3}(kms|hs-crypto):(.*:){2}[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}:key:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.existing_kms_key_crn)),
+      can(regex("^crn:(.*:){3}(kms):(.*:){2}[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}:key:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.existing_kms_key_crn)),
       var.existing_kms_key_crn == null,
     ])
-    error_message = "The provided KMS key CRN in the input 'existing_kms_key_crn' in not valid."
+    error_message = "The provided Key Protect key CRN in the input 'existing_kms_key_crn' is not valid."
   }
 
   validation {
